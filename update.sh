ef97a706c1b7024961598cdf138500909ee5cc8d
HansF <fraiponts@gmail.com>
2021-01-06 00:48:59 +0000

    Deploying to gh-pages from master @ 8a858f7ed1b6452cdad612118783e90ba9c8035d 🚀

--- update.sh
+++ update.sh
@@ -2,7 +2,7 @@
 # Proper header for a Bash script.
 
 echo $1
-sed 's/commitmessage/'.$1.'/g' index.html.template > output.file
+sed 's/commitmessage/'$1'/g' index.html.template > index.html
 git add -A
 git commit -m $1
 git push
